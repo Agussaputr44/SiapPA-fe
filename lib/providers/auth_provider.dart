d36f883e163ba253bf762319906b34fdc749473b
Agus <agusptr44@gmail.com>
2025-06-20 23:17:15 +0700

    add provider auth

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../services/auth_service.dart';
+
+class AuthProvider with ChangeNotifier {
+  String? _token;
+  final AuthService _authService = AuthService();
+
+  String? get token => _token;
+
+  Future<void> login(String email, String password) async {
+    _token = await _authService.login(email, password);
+    await _saveTokenToStorage(_token!);
+    notifyListeners();
+  }
+
+  Future<void> register(String name, String email, String password, String phone) async {
+    await _authService.register(name, email, password, phone);
+  }
+
+  Future<void> logout() async {
+    _token = null;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+    notifyListeners();
+  }
+
+  Future<void> _saveTokenToStorage(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('token', token);
+  }
+
+  Future<void> loadTokenFromStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+    _token = prefs.getString('token');
+    notifyListeners();
+  }
+}
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:siappa/configs/api_config.dart';
+
+class AuthService {
+  Future<String> login(String email, String password) async {
+    final url = Uri.parse(ApiConfig.buildUrl(ApiConfig.login));
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['token'];
+    } else {
+      throw Exception('Login gagal: ${response.body}');
+    }
+  }
+
+  Future<void> register(
+      String name, String email, String password, String phone) async {
+    final url = Uri.parse(ApiConfig.buildUrl(ApiConfig.register));
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'name': name,
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode != 201) {
+      throw Exception('Registrasi gagal: ${response.body}');
+    }
+  }
+}
